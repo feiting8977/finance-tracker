23403acb5babeba41a9d4f215f869486edf6e76f
ting fei <appcareer2018@gmail.com>
2018-06-12 23:16:02 +0000

    Finish finance tracker app

--- db/migrate/20180611225337_create_friendships.rb
+++ db/migrate/20180611225337_create_friendships.rb
@@ -2,7 +2,7 @@ class CreateFriendships < ActiveRecord::Migration[5.2]
   def change
     create_table :friendships do |t|
       t.belongs_to :user
-      t.belongs_to :friend, class: 'user'
+      t.belongs_to :friend, class: 'User'
       t.timestamps
     end
   end
